778b7de0aaf78e2af4f21cc9df29803caf1bda65
Ajay <majay1638@gmail.com>
2023-03-31 14:27:12 +0530

    added some changes

--- server.js
+++ server.js
@@ -18,17 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configure Cloudinary
 // Configure Multer to handle file uploads
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-});
-
-var upload = multer({ storage: storage });
-
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 // Routers
 const userRouter = require("./router/userRouter")
